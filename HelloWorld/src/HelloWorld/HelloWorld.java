c9f9858a179dd5e50711f8560f8eb216887cf446
1160300906 <36832774+1160300906@users.noreply.github.com>
2018-07-23 22:27:31 +0800

    Add files via upload

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package HelloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) //程序入口main()的方法
+	  {
+	    System.out.println("Hello my World!");//输出字符串Hello World!
+	  }
+}
